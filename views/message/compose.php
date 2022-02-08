14b15e0d847bc559a2775cf3f0003ede25e02c4f
Nico Ratti <rattinico92@gmail.com>
2022-02-08 08:20:14 +0100

    fix phpdoc

--- views/message/compose.php
+++ views/message/compose.php
@@ -6,7 +6,14 @@ use yii\widgets\ActiveForm;
 use niciz\message\models\Message;
 
 /* @var $this yii\web\View */
-/* @var $model app\models\Message */
+/* @var $model Message */
+/* @var $answers string|null */
+/* @var $draft_hash */
+/* @var $origin */
+/* @var $context */
+/* @var $dialog bool */
+/* @var $allow_multiple bool */
+/* @var $possible_recipients array */
 
 if ($answers) {
     $this->title = Yii::t('message', 'Answer message');
@@ -18,111 +25,111 @@ $this->params['breadcrumbs'][] = $this->title;
 
 rmrevin\yii\fontawesome\AssetBundle::register($this);
 ?>
-<div class="message-create">
+    <div class="message-create">
 
-    <?php if (!$dialog): ?>
-        <?= $this->render('_actions'); ?>
-
-        <hr>
-    <?php endif ?>
-
-    <div class="message-form">
-
-        <?php $form = ActiveForm::begin(['id' => 'message-form']); ?>
-
-        <div class="row">
-            <div class="col-md-4">
-                <?php
-                if ($model->to) {
-                    if ($model->to && is_array($model->to) && count($model->to) === 1) {
-                        $to = $model->to[0];
-                    } else {
-                        $to = json_encode($model->to);
-                    }
-
-                    $recipient_label = Yii::t('message', 'Recipient');
-                    echo "<label class=\"control-label\" for=\"message-recipient\">$recipient_label</label>";
-                    $caption_attribute = Message::determineUserCaptionAttribute();
-
-                    if (is_callable($caption_attribute)) {
-                        $caption_attribute = call_user_func($caption_attribute, $model->recipient);
-                        echo '<p>' . $caption_attribute . '</p>';
-                    } else {
-                        echo '<p>' . $model->recipient->username . '</p>';
-                    }
-                    echo '<input type=hidden name="Message[to]" value="' . $to . '" />';
-                } else
-                    echo $form->field($model, 'to')->widget(Select2::class, [
-                        'data' => $possible_recipients,
-                        'showToggleAll' => false, # avoid accidental or malicious spam
-                        'options' => [
-                            'multiple' => $allow_multiple,
-                            'placeholder' => Yii::t('message', $allow_multiple ? 'Choose one or more recipients' : 'Choose the recipient'),
-                        ],
-                        'language' => Yii::$app->language ? Yii::$app->language : null,
-                    ]); ?>
-            </div>
-            <div class="col-md-8">
-                <?= $form->field($model, 'title')->textInput(['maxlength' => true, 'required' => 'required']) ?>
-            </div>
-        </div>
-
-        <?php if ($answers && $origin) { ?>
-
-            <p> <?= Yii::t('message', 'Original message'); ?> </p>
-
-            <?= $origin->message; ?>
+        <?php if (!$dialog): ?>
+            <?= $this->render('_actions'); ?>
 
             <hr>
+        <?php endif ?>
 
-        <?php } ?>
+        <div class="message-form">
 
-        <?= $form->field($model, 'message')->textarea(['rows' => 6]) ?>
+            <?php $form = ActiveForm::begin(['id' => 'message-form']); ?>
 
-        <?= $form->field($model, 'context')->hiddenInput()->label(false); ?>
+            <div class="row">
+                <div class="col-md-4">
+                    <?php
+                    if ($model->to) {
+                        if ($model->to && is_array($model->to) && count($model->to) === 1) {
+                            $to = $model->to[0];
+                        } else {
+                            $to = json_encode($model->to);
+                        }
 
-        <div class="form-group">
+                        $recipient_label = Yii::t('message', 'Recipient');
+                        echo "<label class=\"control-label\" for=\"message-recipient\">$recipient_label</label>";
+                        $caption_attribute = Message::determineUserCaptionAttribute();
+
+                        if (is_callable($caption_attribute)) {
+                            $caption_attribute = call_user_func($caption_attribute, $model->recipient);
+                            echo '<p>' . $caption_attribute . '</p>';
+                        } else {
+                            echo '<p>' . $model->recipient->username . '</p>';
+                        }
+                        echo '<input type=hidden name="Message[to]" value="' . $to . '" />';
+                    } else
+                        echo $form->field($model, 'to')->widget(Select2::class, [
+                            'data' => $possible_recipients,
+                            'showToggleAll' => false, # avoid accidental or malicious spam
+                            'options' => [
+                                'multiple' => $allow_multiple,
+                                'placeholder' => Yii::t('message', $allow_multiple ? 'Choose one or more recipients' : 'Choose the recipient'),
+                            ],
+                            'language' => Yii::$app->language ? Yii::$app->language : null,
+                        ]); ?>
+                </div>
+                <div class="col-md-8">
+                    <?= $form->field($model, 'title')->textInput(['maxlength' => true, 'required' => 'required']) ?>
+                </div>
+            </div>
+
+            <?php if ($answers && $origin) { ?>
+
+                <p> <?= Yii::t('message', 'Original message'); ?> </p>
+
+                <?= $origin->message; ?>
+
+                <hr>
+
+            <?php } ?>
+
+            <?= $form->field($model, 'message')->textarea(['rows' => 6]) ?>
+
+            <?= $form->field($model, 'context')->hiddenInput()->label(false); ?>
+
+            <div class="form-group">
+                <?php if (!$dialog) { ?>
+
+                <div class="btn-group">
+                    <?php
+                    echo Html::submitButton(
+                        '<i class="fa fa-floppy-o"></i> ' . Yii::t('message', 'Save as Draft'), [
+                        'name' => 'save-as-draft',
+                        'class' => 'btn btn-success btn-draft']);
+
+                    echo Html::submitButton(
+                        '<i class="fa fa-floppy-o"></i> ' . Yii::t('message', 'Save as Template'), [
+                        'name' => 'save-as-template',
+                        'style' => 'margin-left: 5px;',
+                        'class' => 'btn btn-success btn-template']); ?>
+                    <?php } ?>
+                </div>
+
+                <?php
+                echo Html::submitButton(
+                    '<i class="fa fa-envelope"></i> ' . Yii::t('message', 'Send'), [
+                    $dialog ? null : 'data-confirm' => Yii::t('message', 'Are you sure you want to sent this message?'),
+                    'name' => 'send-message',
+                    'class' => 'btn btn-success btn-send-message pull-right']);
+                ?>
+            </div>
+
+            <input type="hidden" name="draft-hash" value="<?= $draft_hash; ?>">
+            <input type="hidden" name="save-draft-url" value="<?= \yii\helpers\Url::to(
+                ['manage-draft', 'hash' => $draft_hash]
+            ); ?>">
+
+            <?php ActiveForm::end(); ?>
+
             <?php if (!$dialog) { ?>
-
-            <div class="btn-group">
-                <?php
-                echo Html::submitButton(
-                    '<i class="fa fa-floppy-o"></i> ' . Yii::t('message', 'Save as Draft'), [
-                    'name' => 'save-as-draft',
-                    'class' => 'btn btn-success btn-draft']);
-
-                echo Html::submitButton(
-                    '<i class="fa fa-floppy-o"></i> ' . Yii::t('message', 'Save as Template'), [
-                    'name' => 'save-as-template',
-                    'style' => 'margin-left: 5px;',
-                    'class' => 'btn btn-success btn-template']); ?>
-                <?php } ?>
-            </div>
-
-            <?php
-            echo Html::submitButton(
-                '<i class="fa fa-envelope"></i> ' . Yii::t('message', 'Send'), [
-                $dialog ? null : 'data-confirm' => Yii::t('message', 'Are you sure you want to sent this message?'),
-                'name' => 'send-message',
-                'class' => 'btn btn-success btn-send-message pull-right']);
-            ?>
+                <hr>
+                <?php echo Html::a('<i class="fa fa-arrow-left" aria-hidden="true"></i> '
+                    . Yii::t('message', 'Back to Inbox'), ['/message/message/inbox']) ?>
+            <?php } ?>
         </div>
 
-        <input type="hidden" name="draft-hash" value="<?= $draft_hash; ?>">
-        <input type="hidden" name="save-draft-url" value="<?= \yii\helpers\Url::to(
-            ['manage-draft', 'hash' => $draft_hash]
-        ); ?>">
-
-        <?php ActiveForm::end(); ?>
-
-        <?php if (!$dialog) { ?>
-            <hr>
-            <?php echo Html::a('<i class="fa fa-arrow-left" aria-hidden="true"></i> '
-                . Yii::t('message', 'Back to Inbox'), ['/message/message/inbox']) ?>
-        <?php } ?>
     </div>
-
-</div>
 
 <?php $this->registerJs(<<<JS
 function saveDraft() {
